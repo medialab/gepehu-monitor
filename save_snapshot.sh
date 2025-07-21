f5d1e55edf5f01bd2907bc8428753ebc4fab7590
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2025-07-21 16:06:23 +0200

    gzip data that could grow quickly too big otherwise

--- save_snapshot.sh
+++ save_snapshot.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+# Stop script if any command fails
 set -eo pipefail
 
+# Do not run script twice in parallel if it takes too much time
 LOCK=/tmp/gpu-monitor.lock
-
 if [ -e "$LOCK" ]; then
   echo "Lock file already present, skipping run..."
   exit 1
 fi
 touch $LOCK
 
+# Prepare directories
 cd $(dirname $0)
-
 mkdir -p data/json
 
+# Enable python environment
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-
 pyenv activate gpuview
 
-dt=$(date +%Y%m%d%H%M%S)
-gpustat -a --json > data/json/${dt}.json && python parse_json.py data/json/${dt}.json
+# Run stats collection + parsing + csv.gz dump
+JSONFILE=data/json/$(date +%Y%m%d%H%M%S).json
+gpustat -a --json > $JSONFILE
+if ls data | grep .csv.gz > /dev/null; then
+  gunzip data/*.csv.gz
+fi
+python parse_json.py $JSONFILE
+gzip data/*.csv
 
+# Cleanup
+rm -f $JSONFILE
 rm -f $LOCK
